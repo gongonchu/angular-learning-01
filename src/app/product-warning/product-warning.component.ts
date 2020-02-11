881ed5a1527dc765ddb3a8b5ae5ee0542c4529ff
gongonchu <gtombette@gmail.com>
2020-02-11 11:49:45 +0100

    Replace alert by warning

---
+++ src/app/product-warning/product-warning.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product-warning',
+  templateUrl: './product-warning.component.html',
+  styleUrls: ['./product-warning.component.css']
+})
+export class ProductWarningComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
